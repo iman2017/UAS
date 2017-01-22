067d06053b2d32dab5cc301f14ba02583b1d55bd
Iman2017 <imanmuliawan@gmail.com>
2017-01-23 00:00:11 +0700

    hitung kedua

---
+++ HitungLuas.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*package Hitung;
+*/
+/**
+ *
+ * @author Team IT
+ */
+public class HitungLuas {
+    public static void main(String[] args) {
+        int panjang = 7;
+        int lebar = 6;
+        
+        System.out.println("Panjangnya "+panjang);
+        System.out.println("Lebarnya "+lebar);
+        System.out.println("Luasnya adalah "+panjang*lebar);
+    }
+    
+}
