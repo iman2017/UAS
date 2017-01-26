83af75c61c93df21e8453709745bd280590b650d
Iman Muliawan <imanmuliawan@gmail.com>
2017-01-27 00:53:21 +0700

    coba langkah merge

--- HitungLuas2.java
+++ HitungLuas2.java
@@ -7,7 +7,6 @@ public class HitungLuas2 {
 	int tinggi = 8;
         int Luaspersegi = LuasPersegiPanjang(panjang, lebar);
         int Luassegitiga = LuasSegitiga(setengah, alas, tinggi);
-
         System.out.println("Panjangnya "+panjang);
         System.out.println("Lebarnya "+lebar);
         System.out.println("Luasnya Persegi Panjang adalah "+ Luaspersegi);
@@ -16,13 +15,11 @@ public class HitungLuas2 {
 	System.out.println("Tinggi adalah       "+ tinggi);
 	System.out.println("Luasnya Segitiga adalah "+ Luassegitiga);
     }
-
 public static int LuasPersegiPanjang(int panjang, int lebar) {
         int luas;
         luas = panjang*lebar;
         return luas;
     } 
-
 public static int LuasSegitiga(int setengah, int alas, int tinggi)   {
 	int luas;
 	luas = alas*tinggi/2;
