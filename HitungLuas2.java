ea9138f86ed293cd897e408fe95a40c622f4ac94
Iman Muliawan <imanmuliawan@gmail.com>
2017-01-27 00:27:45 +0700

    ditambah method mencari luas segitiga

--- HitungLuas2.java
+++ HitungLuas2.java
@@ -2,16 +2,30 @@ public class HitungLuas2 {
     public static void main(String[] args) {
         int panjang = 7;
         int lebar = 6;
-        int Luas = LuasPersegiPanjang(panjang, lebar);
-        
+	int setengah = 1/2;
+	int alas = 5;
+	int tinggi = 8;
+        int Luaspersegi = LuasPersegiPanjang(panjang, lebar);
+        int Luassegitiga = LuasSegitiga(setengah, alas, tinggi);
+
         System.out.println("Panjangnya "+panjang);
         System.out.println("Lebarnya "+lebar);
-        System.out.println("Luasnya adalah "+ Luas);
+        System.out.println("Luasnya Persegi Panjang adalah "+ Luaspersegi);
+	System.out.println("");
+	System.out.println("Panjang Alas adalah "+ alas);
+	System.out.println("Tinggi adalah       "+ tinggi);
+	System.out.println("Luasnya Segitiga adalah "+ Luassegitiga);
     }
 
 public static int LuasPersegiPanjang(int panjang, int lebar) {
         int luas;
         luas = panjang*lebar;
         return luas;
-    }    
+    } 
+
+public static int LuasSegitiga(int setengah, int alas, int tinggi)   {
+	int luas;
+	luas = alas*tinggi/2;
+	return luas;
+    } 
 }
